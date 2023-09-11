224676bf8c64ef0d23bb90e8b3315144f32fe15e
Jesus Torres Dominguez <jesustorresdominguez@Jesuss-MacBook-Air-3.local>
2023-09-11 17:22:23 -0600

    modified the switch to add the arrow option with a t and change the triagle to a T

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = T, Square = s, Chars = c, Arrow = t) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -16,7 +16,7 @@ int main()
 
     // Given the user answer, select which method to call
     switch (c) {
-    case 't':
+    case 'T':
       puts("You selected triangle:");
       print_triangle(5, 7);
       break;
@@ -27,7 +27,11 @@ int main()
     case 'c':
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+	print_char_5x7(c);
+      break;
+    case 't':
+      puts("You selected arrow:");
+      print_arrow();
       break;
     case 'q':
       puts("Bye!");
